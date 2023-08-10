58110e6c1e4f09dc0aaf64e5902db3aad2cba3ef
Crystal Secaira <crystalsecaira@gmail.com>
2023-08-09 21:51:50 -0400

    Adding button functionality

---
+++ main.js
@@ -0,0 +1,9 @@
+
+let addButton = document.querySelector('button');
+
+function buttonActions() {
+    addButton.style.backgroundColor = '#550d07';
+    addButton.style.color = '#f2dfce'; 
+}
+
+addButton.onclick = buttonActions; 
